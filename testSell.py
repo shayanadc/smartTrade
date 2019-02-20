73d1fbdad69f9521787610d458704a7b95012b07
shayanadc <shayanadc@shayans-MacBook-Pro.local>
2019-02-20 13:53:44 +0330

    change test case name

--- testSell.py
+++ testSell.py
@@ -4,12 +4,13 @@ import Sell
 from unittest_data_provider import data_provider
 
 class TestSell(TestCase):
-    colors = lambda: (
+    conditions = lambda: (
         ({'buy' :1000, 'benefit': 10, 'stop' : 5, 'bid' : [1100,1010,999,998]}, True),
         ({'buy' :1000, 'benefit': 10, 'stop' : 5 , 'bid' : [955,900,400]}, False),
         ({'buy' :1000, 'benefit': 10, 'stop' : 5, 'bid' : [800,7000,600,500]}, True),
     )
-    @data_provider(colors)
+
+    @data_provider(conditions)
     def test_plan_sell_for_coins_you_already_own_with_market(self,input,expected):
 
         buy_price = input['buy']
